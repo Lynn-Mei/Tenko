222c7f49da5d47eb5c1f47ff0771fd1e5d41c715
Hugo-Mrula <hugo.murura@gmail.com>
2025-04-07 06:52:52 +0200

    Adding activities and updating gradle

---
+++ app/src/main/java/com/nihonium/tenko/BookshelfActivity.kt
@@ -0,0 +1,11 @@
+package com.nihonium.tenko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookshelfActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookshelf)
+    }
+}
---
+++ app/src/main/java/com/nihonium/tenko/handler/Camera.kt
@@ -0,0 +1,5 @@
+package com.nihonium.tenko.handler
+
+class Camera {
+
+}
